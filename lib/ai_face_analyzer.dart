1afa755c70a8a83f4146a4c363685a70957abcbb
vijayisonwork <vijayisonwork@gmail.com>
2026-07-24 14:35:51 +0530

    Initial plugin structure

--- lib/ai_face_analyzer.dart
+++
@@ -1,8 +0,0 @@
-
-import 'ai_face_analyzer_platform_interface.dart';
-
-class AiFaceAnalyzer {
-  Future<String?> getPlatformVersion() {
-    return AiFaceAnalyzerPlatform.instance.getPlatformVersion();
-  }
-}
